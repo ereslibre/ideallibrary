6d7b20dbac25ea93208da131de6ca87f949257ac
Rafael Fernández López <ereslibre@ereslibre.es>
2011-03-06 22:44:48 +0100

    Add Iterator and ConstIterator interfaces, that should be implemented in all data structures in which makes sense to iterate.

---
+++ src/core/interfaces/const_iterator.h
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the Ideal Library
+ * Copyright (C) 2010 Rafael Fernández López <ereslibre@ereslibre.es>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef CONST_ITERATOR_H
+#define CONST_ITERATOR_H
+
+namespace IdealCore {
+
+template <typename T>
+class ConstIterator
+{
+public:
+    virtual ~ConstIterator() {}
+
+    virtual bool hasNext() const = 0;
+    virtual const T &next() = 0;
+    virtual void rewind() = 0;
+};
+
+}
+
+#endif //CONST_ITERATOR_H
